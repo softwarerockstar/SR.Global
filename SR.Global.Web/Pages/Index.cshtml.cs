896af0fd5b48ed6fccb84e6bd15b08a1d7671c8a
Muhammad Haroon <mharoon@gmail.com>
2023-05-13 22:02:30 -0500

    Add project files.

---
+++ SR.Global.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Mime;
+using System.Reflection;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IWebHostEnvironment;
+
+namespace SR.Global.Pages
+{
+    [ValidateAntiForgeryToken]
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IWebHostEnvironment _host;
+
+        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment hostingEnvironment)
+        {
+            _logger = logger;
+            _host = hostingEnvironment;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            Debug.WriteLine(Request.Form["first_name"]);
+            Debug.WriteLine(Request.Form["last_name"]);
+            Debug.WriteLine(Request.Form["email"]);
+
+            
+
+            var path = Path.Combine(_host.WebRootPath, "content\\flyer.pdf");
+            var content = await System.IO.File.ReadAllBytesAsync(path);
+
+            var fileName = Path.GetRandomFileName() + ".pdf";
+            return File(content, "application/pdf", fileName);
+
+
+        }
+    }
+}
